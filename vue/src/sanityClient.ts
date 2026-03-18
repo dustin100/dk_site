8a0bf049394dda8d2199415da378c2a332d0fdbc
Dustin Kelly <dmkelly100@gmail.com>
2026-03-18 15:09:24 -0400

    BUILD: Vue init with tailwind

---
+++ vue/src/sanityClient.ts
@@ -0,0 +1,8 @@
+import { createClient } from '@sanity/client';
+
+export const client = createClient({
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
+  useCdn: import.meta.env.VITE_SANITY_USE_CDN === 'true',
+  apiVersion: import.meta.env.VITE_SANITY_API_VERSION,
+});
